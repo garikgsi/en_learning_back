6609f89d9eb979cda7d55c6d96a6ab5d7eebfa15
Igor Gorbachev <i.gorbachev@naukanet.ru>
2026-07-31 15:14:07 +0300

    development files

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,8 +5,18 @@ if [ ! -f vendor/autoload.php ]; then
     composer install --no-interaction --prefer-dist
 fi
 
-mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views
-chown -R www-data:www-data storage bootstrap/cache
+mkdir -p \
+    storage/app/private \
+    storage/app/public \
+    storage/framework/cache \
+    storage/framework/sessions \
+    storage/framework/views \
+    storage/logs
+
+if [ "$(id -u)" = "0" ]; then
+    chown -R www-data:www-data storage bootstrap/cache
+fi
+
 chmod -R ug+rwX storage bootstrap/cache
 
 exec "$@"
